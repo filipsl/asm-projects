e591515fab8e46bca6ca9a46107eafff004a426f
filipsl <filip.slazyk@gmail.com>
2019-03-09 13:14:25 +0100

    Define basic segments of the program

--- 01/project_01.asm
+++ 01/project_01.asm
@@ -1,1 +1,27 @@
-;hello world
+;Project 01 - verbal calculator
+;Created by Filip SLazyk
+;AGH UST 2019
+
+
+;segment danych
+data1 segment
+
+hello_msg db "Enter a description of a calculation: $"
+result_ms db "The result is: $"
+
+data1 ends
+
+
+;segment programu
+code1 segment
+
+start:
+
+code1 ends
+
+
+;inicjalizacja stosu
+stack1 segment STACK
+    dw 200 dup(?) ;wypelnij stos 200 dowolnymi slowami
+top dw ?          ;okresla wierzcholek stostu
+stack1 ends
